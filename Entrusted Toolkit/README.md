1545df03c39e58d46ab444001d079e35d254af54
John Hoerr <jhoerr@gmail.com>
2018-06-08 10:53:09 -0400

    refactor `verifyName` as `auditFolderName`

--- Entrusted Toolkit/README.md
+++ Entrusted Toolkit/README.md
@@ -4,8 +4,9 @@ These script enable admins to scan and modify institutional data in 'entrusted' 
 
 | Script        | Description |
 | ------------- |-------------|
-| [printUserFolders](#printUserFolders) | Given a Box folder ID, log the ID and path of that folder and every subfolder. |
-| [ensureFolderName](#ensureFolderName) | Given a Box folder ID and some prefix, rename folders such that the folder and every subfolder begins with the specified prefix. |
+| [printUserFolders](#printUserFolders) | List all subfolders of a given folder |
+| [ensureFolderName](#ensureFolderName) | Ensure that all subfolder names start with a specified prefix |
+| [auditFolderName](#auditFolderName) | Print the path of any subfolder whose name does not start with a specified prefix |
 
 
 ## printUserFolders
@@ -55,3 +56,23 @@ Box Folder Tree After:
   /[Sensitive] Folder A
   /[Sensitive] Folder B
 ```
+
+## auditFolderName
+
+Given a Box folder ID and some list of prefix, print the ID and path of any subfolder whose name does not start with one of the prefixes.
+
+### Usage
+
+`printUserFolders -f FOLDER_ID -u USER_ID -p PREFIX_1 PREFIX_2 -a 'PATH_TO_AUTH_CONFIG_JSON'`
+
+### Example
+
+`printUserFolders -f 1234509876 -u 112233 -p [Sensitive] {Sensitive} -a '~/Documents/Box_Jwt_Config.json'`
+
+Output:
+
+```
+Violation 12345 /My Files
+Violation 12346 /My Files/Folder A
+Violation 12347 /My Files/Folder B
+```   
---
+++ Entrusted Toolkit/auditFolderName.py
@@ -0,0 +1,31 @@
+import sys
+from argparse import ArgumentParser
+from common import authenticate_as_user, walk_folder_tree, folder_str
+
+## Parse command line arguments.
+parser = ArgumentParser(description="Given a Box folder ID and some list of prefix, print the path of any subfolder whose name does not start with one of the prefixes.")
+parser.add_argument("-f", "--folder", dest="folder",     required=True, help="ID of top-level Box folder", metavar="FOLDER_ID")
+parser.add_argument("-u", "--user",   dest="user",       required=True, help="ID of the user that owns or has write permissions on the folder", metavar="USER_ID")
+parser.add_argument("-p", "--prefixes", dest="prefixes", required=True, nargs='+', help="The acceptable folder name prefixes", metavar="PREFIX")
+parser.add_argument("-c", "--config", dest="config",     required=True, help="path to the JSON file containing your JWT authorization configuration. For formatting information, see: https://developer.box.com/docs/setting-up-a-jwt-app#section-use-an-application-config-file", metavar="PATH")
+args = parser.parse_args()
+
+def audit_name_starts_with(prefixes):
+    """
+    Given a list of prefixes, returns a function that takes a folder and prints the folder path if the folder name does not start with one of the prefixes.
+    """
+    def action(folder):
+        if any(folder.name.startswith(prefix) for prefix in prefixes) == False:
+            print(f"{folder_str(folder)}")
+    return action
+
+
+# Authenticate and get client instance
+client = authenticate_as_user(args.config, args.user)
+
+# Get the folder for the given folder ID. This is where we will start our walk.
+root = client.folder(folder_id=args.folder).get()
+# Walk the folder tree, print the path of each folder.
+walk_folder_tree(client, root, audit_name_starts_with(args.prefixes))
+
+print(f"Done!")
