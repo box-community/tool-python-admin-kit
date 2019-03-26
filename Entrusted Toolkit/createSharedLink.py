7adcb972ddb4a5526ef40a9ed0c8a2f03fe145e0
John Hoerr <jhoerr@gmail.com>
2019-03-25 20:44:26 -0400

    Update args to match readme

--- Entrusted Toolkit/createSharedLink.py
+++ Entrusted Toolkit/createSharedLink.py
@@ -7,12 +7,12 @@ from argparse import ArgumentParser
 parser = ArgumentParser(description="Given a Box folder ID, create a shared link with the specified expiration (in days) for all files in the folder.")
 parser.add_argument("-f", "--folder", dest="folder", required=True, help="ID of top-level Box folder", metavar="FOLDER_ID")
 parser.add_argument("-e", "--expiration", dest="expiration", nargs='?', const=1, type=int, default=30, help="The shared link expiration in days (default: 30)", metavar="EXPIRATION")
-parser.add_argument("-c", "--config", dest="config", required=True, help="path to the JSON file containing your JWT authorization configuration. For formatting information, see: https://developer.box.com/docs/setting-up-a-jwt-app#section-use-an-application-config-file", metavar="CONFIG")
+parser.add_argument("-a", "--auth", dest="auth", required=True, help="path to the JSON file containing your JWT authorization configuration. For formatting information, see: https://developer.box.com/docs/setting-up-a-jwt-app#section-use-an-application-config-file", metavar="CONFIG")
 args = parser.parse_args()
 
 # Authenticate as service account and get client instance
 # https://github.com/box/box-python-sdk/blob/master/docs/usage/authentication.md#server-auth-with-jwt
-client = authenticate_as_service_account(args.config)
+client = authenticate_as_service_account(args.auth)
 
 # The expiration date for the shared link, in ISO format.
 expiration = (date.today() + timedelta(days=args.expiration)).isoformat()
